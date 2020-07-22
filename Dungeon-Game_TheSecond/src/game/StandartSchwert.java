acde6a9d81e5ecda2f5c389345dcf4958171b887
preuschhi <bmx.lego@gmail.com>
2020-07-22 16:49:25 +0200

    Create StandartSchwert.java

---
+++ Dungeon-Game_TheSecond/src/game/StandartSchwert.java
@@ -0,0 +1,14 @@
+package game;
+
+public class StandartSchwert extends Waffe {
+
+	
+
+	//Konstruktor
+	public StandartSchwert() {
+		super(10, "Schwaches Schwert", 10, false, false, false, true, true, true);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
