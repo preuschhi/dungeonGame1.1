21c341532da79efadcbd1b4bcbe0c873e9854baa
preuschhi <bmx.lego@gmail.com>
2020-07-22 16:48:58 +0200

    Create DiaSchwert.java

---
+++ Dungeon-Game_TheSecond/src/game/DiaSchwert.java
@@ -0,0 +1,12 @@
+package game;
+
+public class DiaSchwert extends Waffe{
+	//Nur beim Händler!!
+	
+	//Konstruktor
+	public DiaSchwert() {
+		super(55, "Diamanten Schwert", 3, false, false, false, false, false, false);
+		// TODO Auto-generated constructor stub
+	}
+
+}
