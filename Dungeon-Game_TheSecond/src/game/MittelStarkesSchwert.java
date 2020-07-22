c721a3567d163b4fc622ca02471b942ab8761a20
preuschhi <bmx.lego@gmail.com>
2020-07-22 16:49:22 +0200

    Create MittelStarkesSchwert.java

---
+++ Dungeon-Game_TheSecond/src/game/MittelStarkesSchwert.java
@@ -0,0 +1,12 @@
+package game;
+
+public class MittelStarkesSchwert extends Waffe {
+
+	
+	//Konstruktor
+	public MittelStarkesSchwert() {
+		super(20, "Mittleres Schwert", 7, false, false, false, false, false, false);
+		// TODO Auto-generated constructor stub
+	}
+
+}
