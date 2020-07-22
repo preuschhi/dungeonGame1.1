6ba55f47f0965655367804f3257a741c4015a292
preuschhi <bmx.lego@gmail.com>
2020-07-22 16:49:10 +0200

    Create GegnerJürgen.java

---
+++ Dungeon-Game_TheSecond/src/game/GegnerJürgen.java
@@ -0,0 +1,39 @@
+package game;
+
+public class GegnerJürgen extends Gegner {
+
+	Waffe waffeGegner;
+
+	// Konstruktor
+	public GegnerJürgen() {
+		super("Jürgen", 45, 1, 10);
+		MittelStarkesSchwert waffeGegnerJuergen = new MittelStarkesSchwert();
+		this.waffeGegner = waffeGegnerJuergen;
+	}
+	
+	// Methoden
+	public Waffe getWaffe () {
+		return this.waffeGegner;
+	}
+	
+	@Override
+	public void angriff() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void heilen() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void heilen(CustomArray heiltrankID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
