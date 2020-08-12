8588eba3bc6e318b8fa11f87fd60c2f8dd53d1ff
preuschhi <bmx.lego@gmail.com>
2020-08-12 17:41:03 +0200

    Create IMethoden.java

---
+++ Dungeon-Game_TheSecond/Dungeon-Game_TheSecond/src/game/IMethoden.java
@@ -0,0 +1,14 @@
+package game;
+
+public interface IMethoden {
+
+	//Methoden
+	
+	public abstract void angriff(); //Angreifen
+	public abstract void heilen();	//Heilen
+	public abstract String getName();	//Um den Namen vom Spieler/ Gegner zu bekommen
+	public abstract int getLeben();//Um die LP vom Gegner/ Spieler zu bekommen
+	public abstract void setLeben(int neueLeben);//Um nach angriffen oder nach dem Heilen die LP zu aktualisieren
+	void heilen(CustomArray heiltrankID);
+	
+}
