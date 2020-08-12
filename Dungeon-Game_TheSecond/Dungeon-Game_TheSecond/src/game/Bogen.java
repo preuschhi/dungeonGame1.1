9447619856c7f27fb0844d426991eeecdef3828c
preuschhi <bmx.lego@gmail.com>
2020-08-12 17:41:30 +0200

    Create Bogen.java

---
+++ Dungeon-Game_TheSecond/Dungeon-Game_TheSecond/src/game/Bogen.java
@@ -0,0 +1,10 @@
+package game;
+
+public class Bogen extends Waffe {
+
+	public Bogen() {
+		super(8, "Bogen", 10, false, false, false, false, false, false);
+		// TODO Auto-generated constructor stub
+	}
+
+}
