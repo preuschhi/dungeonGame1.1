a9bf5ca05174a62cc5bef9836e3a4d75a2d72129
preuschhi <bmx.lego@gmail.com>
2020-08-12 17:44:59 +0200

    Create DiaSchwert.java

---
+++ src/game/DiaSchwert.java
@@ -0,0 +1,12 @@
+package game;
+
+public class DiaSchwert extends Waffe{
+	//Nur beim Händler!!
+	
+	//Konstruktor
+	public DiaSchwert() {
+		super(18, "Diamanten Schwert", 3, false, false, false, false, false, false);
+		// TODO Auto-generated constructor stub
+	}
+
+}
