75f3c2a12a5524e9993814931715535eeca8f0a4
preuschhi <bmx.lego@gmail.com>
2020-08-12 17:44:35 +0200

    Create MittelStarkesSchwert.java

---
+++ src/game/MittelStarkesSchwert.java
@@ -0,0 +1,12 @@
+package game;
+
+public class MittelStarkesSchwert extends Waffe {
+
+	
+	//Konstruktor
+	public MittelStarkesSchwert() {
+		super(12, "Mittleres Schwert", 7, false, false, false, false, false, false);
+		// TODO Auto-generated constructor stub
+	}
+
+}
