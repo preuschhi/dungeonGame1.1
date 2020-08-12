fe5c55008dd30d0f7c9193ac79d3b307691116e3
preuschhi <bmx.lego@gmail.com>
2020-08-12 17:44:26 +0200

    Create StarkesSchwert.java

---
+++ src/game/StarkesSchwert.java
@@ -0,0 +1,12 @@
+package game;
+
+public class StarkesSchwert extends Waffe{
+
+	
+	//Konstruktor
+	public StarkesSchwert() {
+		super(16, "Starkes Schwert", 4, false, false, false, false, false, false);
+		// TODO Auto-generated constructor stub
+	}
+
+}
