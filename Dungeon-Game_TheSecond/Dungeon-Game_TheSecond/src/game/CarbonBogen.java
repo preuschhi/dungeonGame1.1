8ac8c9ebf707b9ef58754d6528e3648d13d44493
preuschhi <bmx.lego@gmail.com>
2020-08-12 17:41:28 +0200

    Create CarbonBogen.java

---
+++ Dungeon-Game_TheSecond/Dungeon-Game_TheSecond/src/game/CarbonBogen.java
@@ -0,0 +1,12 @@
+package game;
+
+public class CarbonBogen extends Waffe{
+
+	//Konstruktor
+	public CarbonBogen() {
+		super(13, "Carbon Bogen", 7.5, false, false, false, false, false, false);
+		// TODO Auto-generated constructor stub
+	}
+
+		
+}
