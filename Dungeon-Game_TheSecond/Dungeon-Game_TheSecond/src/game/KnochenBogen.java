140159065999e866ff48c04ac3b784715f0605d9
preuschhi <bmx.lego@gmail.com>
2020-08-12 17:41:01 +0200

    Create KnochenBogen.java

---
+++ Dungeon-Game_TheSecond/Dungeon-Game_TheSecond/src/game/KnochenBogen.java
@@ -0,0 +1,11 @@
+package game;
+
+public class KnochenBogen extends Waffe{
+
+	//Konstruktor
+	public KnochenBogen() {
+		super(11, "Knochen Bogen", 6, false, false, false, false, false, false);
+		// TODO Auto-generated constructor stub
+	}
+
+}
